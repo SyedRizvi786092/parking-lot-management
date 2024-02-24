dda293c15872fafe6c96c92850ed90728279fe15
Mohd Mojiz Hasan <mohdmojiz.stdnt@srmu.ac.in>
2024-02-24 19:26:34 +0000

    Add Main  Logic

---
+++ src/logic/Slot.java
@@ -0,0 +1,12 @@
+package logic;
+
+public class Slot {
+    String type;
+    Vehicle vehicle;
+    String ticketId;
+    public Slot(String type) {
+        this.type = type;
+        this.vehicle=null;
+        this.ticketId=null;
+    }
+}
